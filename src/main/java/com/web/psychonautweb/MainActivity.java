af1c075329265f70a88017646d5faa0b5e3290f9
Mathieu <matthlima@hotmail.com.br>
2021-02-11 20:32:41 -0300

    v1.0 - Navigation working well. Known bug: Can not open third apps. (i.e external links and youtube).

---
+++ src/main/java/com/web/psychonautweb/MainActivity.java
@@ -0,0 +1,80 @@
+package com.web.psychonautweb;
+
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+    @SuppressLint("SetJavaScriptEnabled")
+    @Override
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        final ProgressBar progressBar = findViewById(R.id.progress);
+        progressBar.setVisibility(View.INVISIBLE);
+        WebView webView = findViewById(R.id.webView);
+        webView.loadUrl("https://psychonautwiki.org/wiki/Main_Page");
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setOnKeyListener(new View.OnKeyListener()
+        {
+            @Override
+            public boolean onKey(View v, int keyCode, KeyEvent event)
+            {
+                if(event.getAction() == KeyEvent.ACTION_DOWN)
+                {
+                    WebView webView = (WebView) v;
+
+                    switch(keyCode)
+                    {
+                        case KeyEvent.KEYCODE_BACK:
+                            if(webView.canGoBack())
+                            {
+                                webView.goBack();
+                                return true;
+                            }
+                            break;
+                    }
+                }
+
+                return false;
+            }
+        });
+
+
+
+
+
+        webView.setWebViewClient(new WebViewClient(){
+            @Override
+            public void onPageStarted(WebView view, String url, Bitmap favicon) {
+                super.onPageStarted(view, url, favicon);
+                progressBar.setVisibility(View.VISIBLE);
+            }
+
+
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                super.onPageFinished(view, url);
+                progressBar.setVisibility(View.INVISIBLE);
+            }
+        });
+
+    }
+}
